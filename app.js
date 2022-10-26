eec5e3fd0e4b420278869bbcea0451e4bb620563
Lael Pierre <lael.aichaoui@gmail.com>
2022-10-25 18:10:23 -0700

    refactored display menu function

--- app.js
+++ app.js
@@ -78,7 +78,11 @@ const menu = [
 const sectionCenter = document.querySelector(".section-center");
 
 window.addEventListener("DOMContentLoaded", function () {
-  let displayMenu = menu.map(function (item) {
+  displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
     // console.log(item);
     return `<article class="menu-item">
         <img src="${item.img}" alt="${item.title}" class="img photo">
@@ -94,4 +98,4 @@ window.addEventListener("DOMContentLoaded", function () {
   displayMenu = displayMenu.join("");
 
   sectionCenter.innerHTML = displayMenu;
-});
+}
